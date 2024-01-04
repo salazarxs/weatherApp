be5f693daacaa79268ddfbfe4d5927a7130b969a
salazarxs <martinpaolo10@gmail.com>
2024-01-04 20:06:45 -0300

    navbar in cities

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Weather from './components/Weather'
 import Settings from './components/Settings'
+import Cities from './components/Cities'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' Component={Weather} />
+        <Route path='/cities' Component={Cities} />
         <Route path='/settings' Component={Settings} />
       </Routes>
 
